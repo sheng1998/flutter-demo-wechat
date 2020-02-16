c5122a41f16c4084964f36da4a7b934b840355c4
sheng <18475140601@163.com>
2020-02-16 13:36:44 +0800

    主页列表绘画

--- lib/home/conversation_page.dart
+++ lib/home/conversation_page.dart
@@ -1,5 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart' show AppColors, AppStyles, Constants;
+import '../modal/conversation.dart' show Conversation, mockConversations;
+
+class _ConversationItem extends StatelessWidget {
+  const _ConversationItem({Key key, this.conversation})
+      : assert(conversation != null),
+      super(key : key);
+
+  final Conversation conversation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Color(AppColors.ConversationItemBg),
+        border: Border(
+          bottom: BorderSide(
+            color: Color(AppColors.DividerColor),
+            width: Constants.DividerWidth,
+          )
+        )
+      ),
+      child: Row(
+        children: <Widget>[
+          Image.asset(
+            'assets/images/default_nor_avatar.png',
+            width: Constants.ContactAvatarSize,
+            height: Constants.ContactAvatarSize,
+          ),
+          Container(width: 10.0,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(conversation.title, style: AppStyles.TitleStyle),
+                Text(conversation.des, style: AppStyles.DesStyle),
+              ],
+            )
+          ),
+          Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(conversation.updateAt, style: AppStyles.DesStyle),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class ConversationPage extends StatefulWidget {
   ConversationPage({Key key}) : super(key: key);
 
@@ -10,8 +63,11 @@ class ConversationPage extends StatefulWidget {
 class _ConversationPageState extends State<ConversationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return _ConversationItem(conversation: mockConversations[index],);
+      },
+      itemCount: mockConversations.length,
     );
   }
 }
